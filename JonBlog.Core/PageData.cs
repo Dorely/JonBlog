e184f5a7e6f37a6bfb30fe5d452ec676cfe9856a
Jonathan Harmon <jont.harmon@gmail.com>
2020-06-01 19:19:24 -0500

    Add project files.

---
+++ JonBlog.Core/PageData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JonBlog.Core
+{
+    public class PageData
+    {
+        public int PageId { get; set; }
+        public string Title { get; set; }
+        public string Html { get; set; }
+        public string Css { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
